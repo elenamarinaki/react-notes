75eabbb7a59a851f19abab1ba12b08de2a129ade
Elena <rhapsodyenmauve@gmail.com>
2021-09-03 02:49:44 +0100

    turn App into Component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,23 +1,46 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello, my name is Elena! You?</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
+//         <p>Hello, my name is Elena! You?</p>
+//         <a
+//           className='App-link'
+//           href='https://reactjs.org'
+//           target='_blank'
+//           rel='noopener noreferrer'
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// instead of making App a function, we turn it into a component
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Hello, my name is Elena! You?</p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <div>This is a `div`</div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
