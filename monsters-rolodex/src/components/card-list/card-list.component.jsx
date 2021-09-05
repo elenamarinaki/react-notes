b50cb497310085c7af31615ee5cb60aacb5b0869
Elena <rhapsodyenmauve@gmail.com>
2021-09-05 03:49:19 +0100

    compartemtalise even more now part of the mapping inside card-list has been transfered into the card component. This way we can style each card, as a separate element.

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = (props) => {
@@ -7,7 +8,7 @@ export const CardList = (props) => {
   return (
     <div className='card-list'>
       {props.monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
       ))}
     </div>
   );
