d1d4959b504949c3d0e10c53d35c39c0654cee02
Elena <rhapsodyenmauve@gmail.com>
2021-09-13 23:58:26 +0100

    search box components implementation

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 // function App() {
@@ -44,10 +45,9 @@ class App extends Component {
     );
     return (
       <div className='App'>
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monsters'
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
         {/* we're going to move this section inside the card-list component, since it's the component's responsibility to generate the list elements...
