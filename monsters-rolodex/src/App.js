9b633cfda516ba7c4e613ec514337953f3bab257
Elena <rhapsodyenmauve@gmail.com>
2021-09-14 03:23:14 +0100

    styling the page

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   // }
   handleChange = (e) => {
     this.setState({ searchField: e.target.value });
-  }
+  };
 
   render() {
     const { monsters, searchField } = this.state;
@@ -54,6 +54,7 @@ class App extends Component {
     );
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
