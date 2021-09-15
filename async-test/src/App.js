64a86fbf4e423b2a49eb3f04a49f18a5d133cb22
Elena <rhapsodyenmauve@gmail.com>
2021-09-16 00:33:53 +0100

    new app to test asynchronous .setState()

---
+++ async-test/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numberNo: 49,
+    };
+  }
+  handleClick = () => {
+    this.setState({ numberNo: this.state.numberNo + 1 });
+  };
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>{this.state.numberNo}</p>
+          <button onClick={this.handleClick}>Update State</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
     this.state = {
       monsters: [],
       searchField: '',
+      // title: '',
     };
 
     // this.handleChange = this.handleChange.bind(this);
